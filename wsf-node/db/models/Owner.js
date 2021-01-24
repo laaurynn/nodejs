3c4186f149a4aaf7989ea19d2aa4b3c3c32ab501
laaurynn <77863194+laaurynn@users.noreply.github.com>
2021-01-24 22:13:07 +0100

    Add files via upload

---
+++ wsf-node/db/models/Owner.js
@@ -0,0 +1,27 @@
+const { DataTypes, Model } = require("sequelize");
+const db = require("../db");
+
+class Owner extends Model {}
+
+Owner.init(
+  {
+    firstname: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+    },
+    lastname: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+    },
+    birthday: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+  },
+  {
+    sequelize: db,
+    modelName: "Owner",
+  }
+);
+
+module.exports = Owner;
